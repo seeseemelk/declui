2a4f431037c9641a1159e9b5c720d041617d180d
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2020-01-22 20:40:18 +0100

    Rearange order of imports

--- source/declui/components/package.d
+++ source/declui/components/package.d
@@ -2,8 +2,8 @@ module declui.components;
 
 public
 {
+	import declui.components.button;
 	import declui.components.component;
 	import declui.components.label;
 	import declui.components.window;
-	import declui.components.button;
 }
